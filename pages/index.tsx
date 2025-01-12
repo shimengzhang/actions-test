b9d55630b7488c8d6b38e1b9df5d643045090c72
shimengzhang666 <shimengzhang666@126.com>
2025-01-12 13:18:48 +0800

    fix: 调整文案

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Get 123 started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               pages/index.tsx
             </code>
